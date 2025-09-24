631fa559ad3311b07c873f2f742367320671603e
Sugeerthan <ssugeerthan18@gmail.com>
2025-09-24 11:50:01 +0530

    finalized sprint 1 front end

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import './App.css';
 import ProfilePage from './components/ProfilePage';
@@ -12,39 +11,3 @@ function App() {
 }
 
 export default App;
-
-
-import React from 'react';
-import './App.css';
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
-import MainContent from './components/MainContent';
-
-function App() {
-  return (
-    <div className="app">
-      <Header />
-      <div className="app-body">
-        <Sidebar />
-        <MainContent />
-      </div>
-    </div>
-  );
-}
-
-export default App;
-
-import './App.css'
-import LoginPage from './LoginPage'
-
-function App() {
-  return (
-    <div className="App">
-      <LoginPage />
-    </div>
-  )
-}
-
-export default App
-
-
